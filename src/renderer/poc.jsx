fd5f7f0afec6f2ca27273d7406df375e799b4c25
Guillermo Vaya <guivaya@gmail.com>
2020-02-11 16:46:42 +0100

    structural changes

---
+++ src/renderer/poc.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class POC extends React.PureComponent {
+
+  render() {
+    return (
+      <div>
+        <Button>Tab1</Button>
+        <Button>Tab2</Button>
+      </div>
+    )
+  }
+}
